23f27fed03f5448b7bd2d96bba7c7df9484a7f9c
MARIA EDUARDA KNUPP RATTO <maria_ratto@estudante.sesisenai.org.br>
2025-02-19 15:26:47 -0300

    Criação da Navbar

--- advocacia/src/App.jsx
+++ advocacia/src/App.jsx
@@ -3,16 +3,18 @@ import CadastroAdvogados from './Cadastro_Advogados/CadastroAdvogados'
 import CadastroUsuario from './Cadastro_Usuario/CadastroUsuario'
 import LoginAdvogados from './Login_Advogados/LoginAdvogados'
 import LoginUsuario from './Login_Usuario/LoginUsuario'
+import Navbar from './Navbar/Navbar'
 
 function App() {
 
 
   return (
     <>
-     <CadastroAdvogados/>
+     {/* <CadastroAdvogados/>
      <CadastroUsuario/>
      <LoginUsuario/>
-     <LoginAdvogados/>
+     <LoginAdvogados/> */}
+     <Navbar />
     </>
   )
 }
---
+++ advocacia/src/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    
+    <nav>
+        <div className='logo-nav'> 
+        <img src="lawofdefense2.jpg" alt="logo" />
+        </div>
+        <div className='links-nav'>
+        <a href=""></a>
+        </div>
+    </nav>
+    
+  )
+}
+
+export default Navbar
