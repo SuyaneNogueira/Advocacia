76dc2ab0a34012b1ddb355af921ec23ac134d641
MANUEL ALEJANDRO MUÑOZ GONZÁLEZ <manuel_gonzalez@estudante.sesisenai.org.br>
2025-02-27 15:57:34 -0300

    Manuel

--- advocacia/src/App.jsx
+++ advocacia/src/App.jsx
@@ -19,8 +19,8 @@ function App() {
      {/* <LoginUsuario/> */}
      {/* <LoginAdvogados/> */}
       {/*<Navbar />*/} 
-     {/*<Slider/>*/}
-     {/*<Maps />*/}
+    <Slider/> 
+    { /* <Maps /> */}
     </>
   )
 }
