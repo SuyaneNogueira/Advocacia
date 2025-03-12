c586f993903e1d24c0862cb577419e68ca2457c7
SUYANE CONCEIÇÃO NOGUEIRA <suyane_nogueira@estudante.sesisenai.org.br>
2025-03-12 15:11:02 -0300

    Suyane

---
+++ advocacia/src/Router/Router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom"; 
+
+// como tem que estar o import
+//  import Contato from "../telaparatodos/Contato";
+
+const router = createBrowserRouter([
+
+// como tem que esta o path 
+// {path: "/", element: <TelaInicialT/>},
+
+
+
+
+])
+
+export default Router
