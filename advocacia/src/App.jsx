c7f896937c2b48bf73738c7157931b407d988cad
MANUEL ALEJANDRO MUÑOZ GONZÁLEZ <manuel_gonzalez@estudante.sesisenai.org.br>
2025-03-17 17:11:37 -0300

    Manuel

--- advocacia/src/App.jsx
+++ advocacia/src/App.jsx
@@ -6,6 +6,7 @@ import Lading_page from './Lading_page/Lading_page'
 import LoginAdvogados from './Login_Advogados/LoginAdvogados'
 import LoginUsuario from './Login_Usuario/LoginUsuario'
 import Navbar from './Navbar/Navbar'
+import PerfilAdvogados from './Perfil_Advogados/PerfilAdvogados'
 import PerfilUsuario from './Perfil_Usuario/PerfilUsuario'
 import Slider from './Slider/Slider'
 import TelaInicial from './Tela_Inicial/TelaInicial'
@@ -15,26 +16,20 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-    <Lading_page/>
-    {/* <Slider/> */}
-    {/* <Maps/> */}
-=======
-<<<<<<< HEAD
-     {/* <TelaInicial/> */}
+    
+      {/*<TelaInicial/>*/} 
+      {/*<Lading_page/>/*}
      {/* <CadastroAdvogados/> */}
      {/* <CadastroUsuario/> */}
      {/* <LoginUsuario/> */}
      {/* <LoginAdvogados/> */}
       {/*<Navbar />*/} 
     {/*<Slider/> */} 
-    { /* <Maps /> */}
-    <PerfilUsuario />
-    {/*< Lading_page />*/}
-=======
-    
->>>>>>> 045d53f29629fb5bd7d10b61af8fa6a6f6002484
->>>>>>> e1c7343ae491fa20d2196f3fab728ed0b219753b
+      {/*<Maps />*/}  
+   {/*<PerfilUsuario />*/}
+   <PerfilAdvogados />
+   
+
     </>
   )
 }
--- advocacia/src/Perfil_Advogados/PerfilAdvogados.jsx
+++ advocacia/src/Perfil_Advogados/PerfilAdvogados.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
+import './PerfilAdvogados.css'
 
 function PerfilAdvogados() {
   return (
-    <div>
-      <div>
-        <div>
-          <p>Nome:</p> <input type="text" name="" id="" />
-        </div>
-        <div>
-          <p>OAB:</p><input type="text" name="" id="" />
-        </div>
-        <div>
-          <p>Especialiadades:</p><input type="text" name="" id="" />
-        </div>
+    <div class="div-all-advogados">
+  <div class="div-conteudo">
+    <div class="div-perfil-advogados">
+      <div class="div-nome-advogado">
+        <p>Nome:</p>
+        <input type="text" name="" id="" />
       </div>
-      <div>
-        <p>Descrição breve:</p><input type="text" name="" id="" />
+      <div class="div-oab-advogado">
+        <p>OAB:</p>
+        <input type="text" name="" id="" />
       </div>
-      <div>
+      <div class="div-especialidades-advogado">
+        <p>Especialidades:</p>
+        <input type="text" name="" id="" />
+      </div>
+      <div class="div-descrição-advogado">
+        <p>Descrição breve:</p>
+        <input type="text" name="" id="" />
+      </div>
+      <div class="div-botao-advogado">
         <button>Salvar</button>
       </div>
     </div>
+    <div class="img-advogado">
+      <img src="img" alt="imagem" />
+    </div>
+  </div>
+</div>
+
   )
 }
 
