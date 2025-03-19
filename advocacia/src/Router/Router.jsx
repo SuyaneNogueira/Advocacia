cfd11adb4c0cb8bd98be4ad3d953b13b30ff26bc
MARIA EDUARDA KNUPP RATTO <maria_ratto@estudante.sesisenai.org.br>
2025-03-19 16:53:29 -0300

    Atualizações das áreas de atuação

--- advocacia/src/Router/Router.jsx
+++ advocacia/src/Router/Router.jsx
@@ -1,16 +1,18 @@
-// import { createBrowserRouter } from "react-router-dom"
-// import CadastroAdvogados from "../Cadastro_Advogados/CadastroAdvogados"
-// import CadastroUsuario from "../Cadastro_Usuario/CadastroUsuario"
-// import LoginUsuario from "../Login_Usuario/LoginUsuario"
+import { createBrowserRouter } from "react-router-dom";
+import CadastroAdvogados from "../Cadastro_Advogados/CadastroAdvogados";
+import CadastroUsuario from "../Cadastro_Usuario/CadastroUsuario";
+import LoginUsuario from "../Login_Usuario/LoginUsuario";
+import Lading_page from "../Lading_page/Lading_page";
 
 
-// const Router = createBrowserRouter([
+const Router = createBrowserRouter([
         
-//     {path: "cadastrar-advogado", element: <CadastroAdvogados/>},
-//     {path: "cadastrar-usuario", element: <CadastroUsuario/>},
-//     {path: "cadastro-login", element: <LoginUsuario/>}
+    {path: "/cadastrar_advogado", element: <CadastroAdvogados/>},
+    {path: "/cadastrar_usuario", element: <CadastroUsuario/>},
+    {path: "/cadastro_login", element: <LoginUsuario/>},
+    {path: "/lading_page", element: <Lading_page/>}
 
-// ])
+])
 
 
-// export default Router
+export default Router
