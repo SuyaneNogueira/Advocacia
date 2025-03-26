4af0fb32168a63e92e9b3734fde3f4c086b13b6c
MANUEL ALEJANDRO MUÑOZ GONZÁLEZ <manuel_gonzalez@estudante.sesisenai.org.br>
2025-03-26 15:35:35 -0300

    Manuel

--- advocacia/src/Router/Router.jsx
+++ advocacia/src/Router/Router.jsx
@@ -1,13 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import CadastroAdvogados from "../Cadastro_Advogados/CadastroAdvogados";
-import CadastroUsuario from "../Cadastro_Usuario/CadastroUsuario";
-import LoginUsuario from "../Login_Usuario/LoginUsuario";
-import Lading_page from "../Lading_page/Lading_page";
-import PerfilAdvogados from "../Perfil_Advogados/PerfilAdvogados";
+import Lading_page from "../Front-End/Lading_page/Lading_page";
+import PerfilAdvogados from "../Front-End/Advogados/Perfil_Advogados/PerfilAdvogados";
+
 
 const Router = createBrowserRouter([
-    {path: "/", element: <Lading_page/>},
-   
+    {path: "/", element: <PerfilAdvogados/>},
+    
     
 ])
 
